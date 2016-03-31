2cd31f99db97057a3fd4b0668d65c085db1710cc
Vivek Patel <vnp229@gmail.com>
2016-03-30 20:19:58 -0500

    added pagination to user page

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -1,6 +1,12 @@
 class TweetsController < ApplicationController
   def index
-    @tweets = Tweet.all
+    @page = params[:page] || "1"
+    if params[:search].nil?
+      @tweets = get_tweets
+    else
+      @tweets = get_tweets_by_search
+    end
+
   end
 
   def show
@@ -10,6 +16,14 @@ class TweetsController < ApplicationController
   private
 
   def get_tweet_at_id
-    Tweet.find_by(id: params["id"].to_i)
+    Tweet.find_by(id: params[:id].to_i)
+  end
+
+  def get_tweets
+    Tweet.limit(25).offset((@page.to_i - 1)*25)
+  end
+
+  def get_tweets_by_search
+    Tweet.where("body LIKE '%#{params[:search]}%'").limit(25).offset((@page.to_i - 1)*25)
   end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,12 +3,14 @@ class UsersController < ApplicationController
   end
 
   def show
+    @page = params[:page] || "1"
     @user = get_user_at_id
+    @tweets = @user.tweets.limit(25).offset((@page.to_i - 1)*25)
   end
 
   private
 
   def get_user_at_id
-    User.find_by(id: params["id"].to_i)
+    User.find_by(id: params[:id].to_i)
   end
 end
